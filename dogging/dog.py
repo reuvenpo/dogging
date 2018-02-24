74bf7479e5d2d817b9c68f52a379b5a05f05ef27
Reuven Podmazo <reuven.podmazo@gmail.com>
2018-02-24 17:18:03 +0200

    added support for computed arg-names (with the ">" prefix)

--- dogging/dog.py
+++ dogging/dog.py
@@ -16,9 +16,8 @@ from .teeth import *
 from .bone import *
 
 __all__ = [
-    'dog', 'ExtraAttributes',
+    'dog', 'ComputedArgNames', 'ExtraAttributes',
     'DEBUG', 'INFO', 'WARN', 'WARNING', 'ERROR', 'FATAL', 'CRITICAL',
-    'doggo',
 ]
 
 # Be explicit about exporting these from the module
@@ -30,6 +29,7 @@ ERROR = ERROR
 FATAL = FATAL
 CRITICAL = CRITICAL
 
+_COMPUTED_ARG_PREFIX = '>'  # Common prefix for computed format arg-names
 _SPECIAL_ARG_PREFIX = '@'  # Common prefix for special format arg-names
 # Names of the special format arg-names
 _ARG_PATHNAME = _SPECIAL_ARG_PREFIX + 'pathname'
@@ -78,32 +78,39 @@ _ALL_ARGS = {
 
 
 def resolve_specification_string(spec):
-    return INFO, spec, ()
+    return INFO, spec, (), ()
 
 
 def resolve_specification_sequence(spec):
     level = INFO
     format_string = None
     extras = []
+    computers = []
 
     for part in spec:
         if isinstance(part, int):
             level = part
         elif isinstance(part, basestring):
             format_string = part
-        elif isinstance(part, ExtraAttributes):
+        elif issubclass(part, ExtraAttributes):
             extras.append(part)
+        elif issubclass(part, ComputedArgNames):
+            computers.append(part)
         else:
-            raise TypeError('unsupported type for sequence specification')
+            raise TypeError(
+                'unsupported type for sequence specification: {}'.format(
+                    part.__name__ if type(part) is type else type(part).__name__
+                )
+            )
 
     if format_string is None:
         raise ValueError('must specify a format string in a sequence specification')
 
-    return level, format_string, extras
+    return level, format_string, extras, computers
 
 
 def resolve_specification_none(_):
-    return None, None, ()
+    return None, None, (), ()
 
 
 SPEC_RESOLVERS = {
@@ -123,13 +130,21 @@ def resolve_specification(spec):
     return resolver(spec)
 
 
-def separate_special_from_regular_arg_names(arg_names):
-    """Split the arg_names into two lists, one of the special, and one of the regular arg-names."""
-    special_prefix = _SPECIAL_ARG_PREFIX
+def separate_arg_names_by_prefix(arg_names, prefix):
     return filter2(
-        (lambda arg_name: arg_name.startswith(special_prefix)),
+        (lambda arg_name: arg_name.startswith(prefix)),
         arg_names
     )
+
+
+def separate_special_arg_names(arg_names):
+    """Split the arg_names into two lists, one of the special, and one of the regular arg-names."""
+    return separate_arg_names_by_prefix(arg_names, _SPECIAL_ARG_PREFIX)
+
+
+def separate_computed_arg_names(arg_names):
+    """Split the arg_names into two lists, one of the computed, and one of the regular arg-names."""
+    return separate_arg_names_by_prefix(arg_names, _COMPUTED_ARG_PREFIX)
 
 
 def check_special_arg_names_support(phase, arg_names, supported):
@@ -182,6 +197,7 @@ class DynamicAttributesBase(object):
      subclasses exported by the library.
     """
     __slots__ = ['__cache', '__builder']
+    __args__ = ()
 
     def __init__(self, builder):
         self.__builder = builder
@@ -214,10 +230,26 @@ class ExtraAttributes(DynamicAttributesBase):
     pass
 
 
-def join_extra_attributes(extras):
-    # ``extras`` is a sequence of ``ExtraAttributes`` subclasses.
-    # The methods of those classes are used to generate attributes
-    #  used by the logging.Formatter.
+class ComputedArgNames(DynamicAttributesBase):
+    """Base class for classes describing dynamic computed format arg-names.
+
+    Your subclass can be passed as an element of a sequence of a
+     logging-phase specification.
+    When building the values of arg-names for a log message's format string,
+     additional arg names will be available to the format string. These
+     arg-names will be the names of your defined methods, prefixed with '>'.
+    Your methods will be called for every logging message whose format string
+     has requested them. That is, per message, only the necessary methods
+     will be called.
+    """
+    pass
+
+
+def join_dynamic_attributes(dynamic_attributes):
+    # ``dynamic_attributes`` is a sequence of ``DynamicAttributesBase``
+    #  subclasses.
+    # The methods of those classes are used to generate dynamic attributes
+    #  used by the library and logging system.
     # We allow specifying multiple such classes, but if any of
     #  their methods conflict, the methods of classes later in
     #  the sequence should override those earlier in the sequence.
@@ -229,16 +261,29 @@ def join_extra_attributes(extras):
     # If you understood what i was doing here without
     #  reading this comment or reading the docs, congrats,
     #  you are a Master of the Dark Arts.
-    return type('JoinedExtras', tuple(reversed(extras)), {})
+    return type('JoinedDynamicAttributes', tuple(reversed(dynamic_attributes)), {})
+
+
+def iter_dynamic_attribute_requested_arg_names(dynamic_attributes):
+    """Iterate all arg names requested by a DynamicAttributesBase subclass."""
+    return chain.from_iterable(
+        cls.__args__
+        for cls
+        in dynamic_attributes.mro()
+        if hasattr(cls, '__args__')
+    )
 
 
 class dog(object):
     __slots__ = (
         '_enter_level', '_enter_format', '_enter_extras',
+        '_enter_computer', '_enter_computed_arg_names',
         '_enter_special_arg_names', '_enter_regular_arg_names',
         '_exit_level', '_exit_format', '_exit_extras',
+        '_exit_computer', '_exit_computed_arg_names',
         '_exit_special_arg_names', '_exit_regular_arg_names',
         '_error_level', '_error_format', '_error_extras',
+        '_error_computer', '_error_computed_arg_names',
         '_error_special_arg_names', '_error_regular_arg_names',
         'logger', '_catch', '_propagate', '_exc_info',
         '_default_ret', '_default_ret_arg',
@@ -265,6 +310,14 @@ class dog(object):
         self._exit_extras = None
         self._error_extras = None
 
+        self._enter_computer = None
+        self._exit_computer = None
+        self._error_computer = None
+
+        self._enter_computed_arg_names = None
+        self._exit_computed_arg_names = None
+        self._error_computed_arg_names = None
+
         self._enter_special_arg_names = None
         self._exit_special_arg_names = None
         self._error_special_arg_names = None
@@ -285,45 +338,51 @@ class dog(object):
         else:
             self._default_ret_arg = {_ARG_RET: self._default_ret}
 
-        self._set_extras_for_all_phases(extras)
+        if extras:
+            self._set_extras_for_all_phases(extras)
         self._resolve_specifications(enter, exit, error)
         self._parse_format_strings()
         self._validate_special_arg_names()
+        self._validate_computed_arg_names()
 
     def _set_extras_for_all_phases(self, extras):
         if not isinstance(extras, Iterable):
             raise TypeError('extras argument must be an iterable')
-        extras = join_extra_attributes(tuple(extras))
+        extras = join_dynamic_attributes(tuple(extras))
         self._enter_extras = extras
         self._exit_extras = extras
         self._error_extras = extras
 
     def _add_extras(self, enter, exit, error):
         def join_extras(cls, more_classes):
-            return join_extra_attributes(tuple(chain((cls,), more_classes)))
+            return join_dynamic_attributes(tuple(chain((cls,), more_classes)))
 
         if enter:
             if self._enter_extras:
                 self._enter_extras = join_extras(self._enter_extras, enter)
             else:
-                self._enter_extras = join_extra_attributes(tuple(enter))
+                self._enter_extras = join_dynamic_attributes(tuple(enter))
         if exit:
             if self._exit_extras:
                 self._exit_extras = join_extras(self._exit_extras, exit)
             else:
-                self._exit_extras = join_extra_attributes(tuple(exit))
+                self._exit_extras = join_dynamic_attributes(tuple(exit))
         if error:
             if self._error_extras:
                 self._error_extras = join_extras(self._error_extras, error)
             else:
-                self._error_extras = join_extra_attributes(tuple(error))
+                self._error_extras = join_dynamic_attributes(tuple(error))
 
     def _resolve_specifications(self, enter, exit, error):
-        self._enter_level, self._enter_format, enter_extra = resolve_specification(enter)
-        self._exit_level, self._exit_format, exit_extra = resolve_specification(exit)
-        self._error_level, self._error_format, error_extra = resolve_specification(error)
+        self._enter_level, self._enter_format, enter_extra, enter_computers = resolve_specification(enter)
+        self._exit_level, self._exit_format, exit_extra, exit_computers = resolve_specification(exit)
+        self._error_level, self._error_format, error_extra, error_computers = resolve_specification(error)
 
         self._add_extras(enter_extra, exit_extra, error_extra)
+
+        self._enter_computer = join_dynamic_attributes(enter_computers) if enter_computers else None
+        self._exit_computer = join_dynamic_attributes(exit_computers) if exit_computers else None
+        self._error_computer = join_dynamic_attributes(error_computers) if error_computers else None
 
     def _get_phases_arg_names(self):
         def _get_format_arg_names(fmt):
@@ -342,24 +401,27 @@ class dog(object):
             if arg_names:
                 check_format_arg_names_no_positional(arg_names)
 
-        def chain_arg_names_with_extra_args(arg_names, extras):
+        def chain_arg_names_with_dynamic_attributes_arg_names(arg_names, dynamic_attributes):
             return chain(
                 arg_names,
-                chain.from_iterable(
-                    extra.__args__
-                    for extra
-                    in extras.mro()
-                    if hasattr(extra, '__args__')
-                )
+                iter_dynamic_attribute_requested_arg_names(dynamic_attributes),
             )
 
         # Add references from extra parameters to arg_name lists
         if self._enter_extras:
-            enter_arg_names = chain_arg_names_with_extra_args(enter_arg_names, self._enter_extras)
+            enter_arg_names = chain_arg_names_with_dynamic_attributes_arg_names(enter_arg_names, self._enter_extras)
         if self._exit_extras:
-            exit_arg_names = chain_arg_names_with_extra_args(exit_arg_names, self._exit_extras)
+            exit_arg_names = chain_arg_names_with_dynamic_attributes_arg_names(exit_arg_names, self._exit_extras)
         if self._error_extras:
-            error_arg_names = chain_arg_names_with_extra_args(error_arg_names, self._error_extras)
+            error_arg_names = chain_arg_names_with_dynamic_attributes_arg_names(error_arg_names, self._error_extras)
+
+        # Add references from computed arg names to arg_names lists
+        if self._enter_computer:
+            enter_arg_names = chain_arg_names_with_dynamic_attributes_arg_names(enter_arg_names, self._enter_computer)
+        if self._exit_computer:
+            exit_arg_names = chain_arg_names_with_dynamic_attributes_arg_names(exit_arg_names, self._exit_computer)
+        if self._error_computer:
+            error_arg_names = chain_arg_names_with_dynamic_attributes_arg_names(error_arg_names, self._error_computer)
 
         return enter_arg_names, exit_arg_names, error_arg_names
 
@@ -369,26 +431,34 @@ class dog(object):
     ):
         # For each logging phase, find which special arg names we would need.
         # Also collect the regular references to check them when wrapping a function.
-        pair_of_frozen_sets = (frozenset(),) * 2
+        three_frozen_sets = (frozenset(),) * 3
 
         def separate_arg_names(fmt, arg_names):
-            return (
-                map(frozenset, separate_special_from_regular_arg_names(arg_names))
-                if fmt is not None
-                else pair_of_frozen_sets
+            if fmt is None:
+                return three_frozen_sets
+            computed, arg_names = (
+                map(frozenset, separate_computed_arg_names(arg_names))
             )
+            special, regular = (
+                map(frozenset, separate_special_arg_names(arg_names))
+            )
+            return computed, special, regular
 
-        self._enter_special_arg_names, self._enter_regular_arg_names = (
+        self._enter_computed_arg_names, self._enter_special_arg_names, self._enter_regular_arg_names = (
             separate_arg_names(self._enter_format, enter_arg_names)
         )
 
-        self._exit_special_arg_names, self._exit_regular_arg_names = (
+        self._exit_computed_arg_names, self._exit_special_arg_names, self._exit_regular_arg_names = (
             separate_arg_names(self._exit_format, exit_arg_names)
         )
 
-        self._error_special_arg_names, self._error_regular_arg_names = (
+        self._error_computed_arg_names, self._error_special_arg_names, self._error_regular_arg_names = (
             separate_arg_names(self._error_format, error_arg_names)
         )
+
+    def _parse_format_strings(self):
+        per_phase_arg_names = self._get_phases_arg_names()
+        self._separate_phase_arg_names_to_categories(*per_phase_arg_names)
 
     def _validate_special_arg_names(self):
         # Check that each phases special-arg-names are suitable for the specific phase.
@@ -398,9 +468,26 @@ class dog(object):
         if self._propagate and _ARG_RET in self._error_special_arg_names:
             raise ValueError('Can not use @ret in error message when allowing error propagation')
 
-    def _parse_format_strings(self):
-        per_phase_arg_names = self._get_phases_arg_names()
-        self._separate_phase_arg_names_to_categories(*per_phase_arg_names)
+    def _validate_computed_arg_names(self):
+        for arg_names, computer in (
+            (self._enter_computed_arg_names, self._enter_computer),
+            (self._exit_computed_arg_names, self._exit_computer),
+            (self._error_computed_arg_names, self._error_computer),
+        ):
+            if any(
+                arg_name[1] == '_'  # arg_name[0] == _COMPUTED_ARG_PREFIX
+                for arg_name
+                in arg_names
+            ):
+                raise ValueError('Computed arg-names should not begin with an underscore')
+
+            supplied_dynamic_attributes = set(attr for attr in dir(computer) if not attr.startswith('_'))
+            if any(
+                arg_name[1:] not in supplied_dynamic_attributes
+                for arg_name
+                in arg_names
+            ):
+                raise ValueError('Not all computed arg name definitions are supplied')
 
     # This function always returns the same value throughout the
     # lifetime of a dog instance
@@ -462,12 +549,18 @@ class dog(object):
         enter_level = self._enter_level
         enter_format = self._enter_format
         enter_extras = self._enter_extras
+        enter_computer = self._enter_computer
+        enter_computed_arg_names = self._enter_computed_arg_names
         exit_level = self._exit_level
         exit_format = self._exit_format
         exit_extras = self._exit_extras
+        exit_computer = self._exit_computer
+        exit_computed_arg_names = self._exit_computed_arg_names
         error_level = self._error_level
         error_format = self._error_format
         error_extras = self._error_extras
+        error_computer = self._error_computer
+        error_computed_arg_names = self._error_computed_arg_names
 
         # Check which phases are required
         need_log_enter = self._enter_format is not None
@@ -515,8 +608,9 @@ class dog(object):
 
         pathname, line = get_caller_pathname_and_line()
 
-        # These function always returns the same value throughout the
+        # These builders always return the same value throughout the
         # lifetime of ``func``:
+
         @run_once
         def build_pathname_arg():
             return {ARG_PATHNAME: pathname}
@@ -535,9 +629,9 @@ class dog(object):
             start_time = None
             end_time = None
             _logger = wrapper.logger
-            log_enter = _partial(log, _logger, enter_level, enter_format, enter_extras)
-            log_exit = _partial(log, _logger, exit_level, exit_format, exit_extras)
-            log_error = _partial(log, _logger, error_level, error_format, error_extras)
+            log_enter = _partial(log, _logger, enter_level, enter_format, enter_extras, enter_computer, enter_computed_arg_names)
+            log_exit = _partial(log, _logger, exit_level, exit_format, exit_extras, exit_computer, exit_computed_arg_names)
+            log_error = _partial(log, _logger, error_level, error_format, error_extras, error_computer, error_computed_arg_names)
 
             @run_once
             def build_func_arguments_args():
@@ -738,22 +832,34 @@ class dog(object):
             logger = logging.getLogger(wrapped_func.__globals__['__name__'])
         elif isinstance(logger, str):
             logger = logging.getLogger(logger)
-        else:
-            logger = logger
         return logger
 
-    def _log(self, logger, level, message, extras, builders):
+    def _log(self, logger, level, message, extras, arg_name_computer, arg_names_to_compute, builders):
         @run_once
-        def builder():
+        def basic_builder():
             arguments = {}
             for build in builders:
                 arguments.update(build())
             return arguments
 
         if extras:
-            extra = extras(builder)  # Instantiate the class
+            extra = extras(basic_builder)  # Instantiate the class
         else:
             extra = None
+
+        if arg_name_computer:
+            arg_name_computer = arg_name_computer(basic_builder)
+
+            def builder():
+                arguments = {
+                    arg_name: getattr(arg_name_computer, arg_name[1:])()
+                    for arg_name
+                    in arg_names_to_compute
+                }
+                arguments.update(basic_builder())
+                return arguments
+        else:
+            builder = basic_builder
 
         logger.log(
             level,
@@ -764,11 +870,11 @@ class dog(object):
 
 
 # A decorative doggo!
-doggo = dog(
-    enter='{{{}.__name__}}: *wag*'.format(_ARG_FUNC),
-    exit='{{{}.__name__}}: whimper...'.format(_ARG_FUNC),
-    error=(CRITICAL, '{{{}.__name__}}: bark! bark!'.format(_ARG_FUNC)),
-)
+# doggo = dog(
+#     enter='{@func.__name__}: *wag*',
+#     exit='{@func.__name__}: whimper...',
+#     error=(CRITICAL, '{@func.__name__}: bark! bark!',
+# )
 
 # Stack dogs for extra fun!
 # doggos = lambda fun: doggo(doggo(doggo(fun)))
